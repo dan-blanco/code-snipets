9408dc3980b701cab5bff56449b5ec6802eab35c
dan-blanco <51581590+dan-blanco@users.noreply.github.com>
2022-06-10 19:28:36 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -12,3 +12,30 @@ df = news_df[["text", "title"]].apply(
             ), axis=1)
 df.columns=["text-compound", "title-compound"]
 df.head()
+
+
+
+
+"""
+Perform a backtest on time series data!
+"""
+
+def model(x):
+    # pretend model.... should be trained, fit return single prediction
+    return x.mean()*1.5
+
+data["back_test_pred"] = \
+    data["data"].shift(1).rolling(window=40, min_periods=40).apply(model)
+
+data[["data","back_test_pred"]].plot()
+
+
+"""
+Create tensor of window data 
+"""
+import numpy as np
+np.array([
+    window_data.ravel()
+    for window_data in list(data["data"].rolling(40))
+    if len(window_data) == 40
+])
