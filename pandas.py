f93100d1d332f566c99cb8babc5c027244bf63de
dan-blanco <51581590+dan-blanco@users.noreply.github.com>
2022-08-16 16:32:45 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -2,40 +2,16 @@
 """
 Apply function to multiple series
 and return a dataframe after processing
+
+pretend do_something returns a dict with a key "result" that we want as new columns
 """
-df = news_df[["text", "title"]].apply(
+df = old_df[["col1", "col2"]].apply(
     lambda x: pd.Series(
         ( # tuple
-            analyzer.polarity_scores(x[0])["compound"],
-            analyzer.polarity_scores(x[1])["compound"]
+            do_something(x[0])["result"],
+            do_something(x[1])["result"]
         )
             ), axis=1)
-df.columns=["text-compound", "title-compound"]
+df.columns=["col1-result", "col2-result"]
 df.head()
 
-
-
-
-"""
-Perform a backtest on time series data!
-"""
-
-def model(x):
-    # pretend model.... should be trained, fit return single prediction
-    return x.mean()*1.5
-
-data["back_test_pred"] = \
-    data["data"].shift(1).rolling(window=40, min_periods=40).apply(model)
-
-data[["data","back_test_pred"]].plot()
-
-
-"""
-Create tensor of window data 
-"""
-import numpy as np
-np.array([
-    window_data.ravel()
-    for window_data in list(data["data"].rolling(40))
-    if len(window_data) == 40
-])
