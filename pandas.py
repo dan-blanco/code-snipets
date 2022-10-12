d375007caf78d5f670d944ebe945bc00d05c147c
dan-blanco <51581590+dan-blanco@users.noreply.github.com>
2022-10-12 11:10:04 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -15,3 +15,28 @@ df = old_df[["col1", "col2"]].apply(
 df.columns=["col1-result", "col2-result"]
 df.head()
 
+
+## Normalize JSON Data
+import pandas as pd
+
+# credit to Oyenike for explode and json_normalize
+
+# read data from URL
+data = pd.read_json("https://the-software-guild.s3.amazonaws.com/techstart-1909/data-files/laureate.json")
+
+# normalize the data
+laur = pd.json_normalize(data["laureates"])
+
+# normalize the prices data
+# explode transforms list like oject to dict 
+prizes = pd.json_normalize(laur["prizes"].explode())
+
+# normalize affiliations. 
+# we end up with some empty list, it causes an error with json_normalize
+# to fix it we apply a function to replace any list with an empty {}
+affilliations = pd.json_normalize(prizes["affiliations"].explode().apply(lambda x: {} if isinstance(x, list) else x))
+
+# example of using pandas to count number of laureates in each country and sort it
+# to help explain the group by and aggregator function, the sql code would return the same thing
+""" SELECT COUNT(*) FROM lauretes GROUP BY bornCountryCode ORDER BY COUNT(*) """
+laur.groupby(['bornCountryCode'])['bornCountryCode'].count().sort_values()
